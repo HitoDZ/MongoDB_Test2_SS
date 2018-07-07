33d7fb2936a7b293a53751d800c891f42c5c2046
Victor Petrovich <dimasheludko1999@gmail.com>
2018-07-07 19:42:49 +0200

    create basic repositories to store clients

---
+++ MongoDB_Test2_SS/PersistanceLayer/IClientsRepository.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using Models;
+
+namespace PersistanceLayer
+{
+    /// <summary>
+    /// CRUD operations with clients
+    /// </summary>
+    public interface IClientsRepository
+    {
+        /// <summary>
+        /// Synchroniously read info from DB and convert
+        /// it into lient class
+        /// </summary>
+        /// <param name="id">Client`s id</param>
+        /// <returns>CLient instance</returns>
+        Client Read(long id);
+        
+        /// <summary>
+        /// Asynchroniously read info from DB and convert
+        /// it into lient class
+        /// </summary>
+        /// <param name="id">Client`s id</param>
+        /// <returns>Task with client instance</returns>
+        Task<Client> ReadAsync(long id);
+    }
+}
---
+++ MongoDB_Test2_SS/PersistanceLayer/MongoClientsRepository.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using DbLayer;
+using Models;
+
+namespace PersistanceLayer
+{
+    public class MongoClientsRepository : IClientsRepository
+    {
+        private IDbProvider _dbProvider;
+
+        //it should be moved from there it the future
+        private string clientsCollectionName = "ClientsCollection";
+
+        public MongoClientsRepository(IDbProvider provider)
+        {
+            _dbProvider = provider;
+        }
+        
+        
+        
+        public Client Read(long id)
+        {
+            var session = _dbProvider.GetSession(clientsCollectionName);
+
+            var client = session.GetClient(id);
+
+            return client;
+        }
+
+        public async Task<Client> ReadAsync(long id)
+        {
+            var session = _dbProvider.GetSession(clientsCollectionName);
+
+            var client = await session.GetClientAsync(id);
+
+            return client;
+        }
+    }
+}
